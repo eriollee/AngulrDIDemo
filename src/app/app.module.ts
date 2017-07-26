ba7f1a0d70f6cc5077a253eb4fe018bcaeebfaa7
eriollee <eriollee@163.com>
2017-07-26 21:53:25 +0800

    使用对象提供器注入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,18 +22,18 @@ import { LoggerService } from './shared/logger.service';
   ],
   providers: [{
     provide:ProductService,
-    useFactory:(logger:LoggerService,isDev)=>{
-    if(isDev){
+    useFactory:(logger:LoggerService,appConfig)=>{
+    if(appConfig.isDev){
       return new ProductService(logger);
     }else{
       return new AnotherProductService(logger);
     }
     },
-    deps:[LoggerService,"IS_DEV_ENV"]
+    deps:[LoggerService,"APP_CONFIG"]
 
 },LoggerService,
   {
-    provide:"IS_DEV_ENV",useValue:false
+    provide:"APP_CONFIG",useValue:{isDev:false}
   }
 ],
   bootstrap: [AppComponent]
