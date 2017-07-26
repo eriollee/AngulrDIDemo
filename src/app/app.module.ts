198a11656edafcd13bf9508bbfccb07d79c70898
eriollee <eriollee@163.com>
2017-07-26 21:42:05 +0800

    使用useFactory 实例化service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { Product1Component } from './product1/product1.component';
 import { ProductService } from './shared/product.service';
+import { AnotherProductService } from './shared/another-product.service';
 import { Product2Component } from './product2/product2.component';
 import { LoggerService } from './shared/logger.service';
 @NgModule({
@@ -19,7 +20,15 @@ import { LoggerService } from './shared/logger.service';
     FormsModule,
     HttpModule
   ],
-  providers: [ProductService,LoggerService],
+  providers: [{provide:ProductService,useFactory:()=>{
+    let logger = new LoggerService;
+    let dev= Math.random()>0.5
+    if(dev){
+      return new ProductService(logger);
+    }else{
+      return new AnotherProductService(logger);
+    }
+  }},LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
